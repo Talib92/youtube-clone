4f1631c1c1b61971f1eb3c1d179cf7cc9a810fc8
Talib Mukhtar <talibmukhtar54@gmail.com>
2024-09-06 18:29:39 +0530

    better ux

--- src/components/ButtonLists.js
+++ src/components/ButtonLists.js
@@ -16,17 +16,17 @@ const ButtonLists = () => {
       <Button name= "College" />
       <Button name= "Comedy" />
       <Button name= "Love" />
-      <Button name= "All" />
-      <Button name= "Cricket" />
-      <Button name= "JavaScript" />
-      <Button name= "News" />
-      <Button name= "Code" />
-      <Button name= "Kashmir" />
-      <Button name= "React" />
-      <Button name= "MERN" />
-      <Button name= "College" />
-      <Button name= "Comedy" />
-      <Button name= "Love" />
+      <Button name= "Birthday" />
+      <Button name= "Flats" />
+      <Button name= "Bengaluru" />
+      <Button name= "Office" />
+      <Button name= "Corporate" />
+      <Button name= "World" />
+      <Button name= "Learn" />
+      <Button name= "Stocks" />
+      <Button name= "Node" />
+      <Button name= "NextJs" />
+      <Button name= "MongoDb" />
     </div>
   )
 }
